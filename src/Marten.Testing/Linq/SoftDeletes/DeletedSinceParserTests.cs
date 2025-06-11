ca9809cfa5befc234c4c5aed06815a0cbf0df63d
Oliver Dunthorne <oliver.dunthorne@myunidays.com>
2025-06-11 11:22:41 +0100

    amended where clause test to cover thai/all cultures

--- src/Marten.Testing/Linq/SoftDeletes/DeletedSinceParserTests.cs
+++ src/Marten.Testing/Linq/SoftDeletes/DeletedSinceParserTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq.Expressions;
-using System.Reflection;
+using System.Threading;
 using Marten.Linq.SoftDeletes;
 using Marten.Schema;
 using Marten.Services;
@@ -16,6 +17,20 @@ namespace Marten.Testing.Linq.SoftDeletes
         private readonly MethodCallExpression _expression;
         private readonly DeletedSinceParser _parser;
 
+        public static TheoryData<CultureInfo> AllCultures
+        {
+            get
+            {
+                var data = new TheoryData<CultureInfo>();
+                var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+                foreach (var culture in cultures)
+                {
+                    data.Add(culture);
+                }
+                return data;
+            }
+        }
+        
         public DeletedSinceParserTests()
         {
             _mapping = new DocumentMapping(typeof(object), new StoreOptions()) {DeleteStyle = DeleteStyle.SoftDelete};
@@ -25,16 +40,21 @@ namespace Marten.Testing.Linq.SoftDeletes
             _parser = new DeletedSinceParser();
         }
 
-        [Fact]
-        public void WhereFragmentContainsExpectedExpression()
+        [Theory]
+        [MemberData(nameof(AllCultures))]
+        public void WhereFragmentContainsExpectedExpression(CultureInfo cultureInfo)
         {
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+            
             var result = _parser.Parse(_mapping, new JsonNetSerializer(), _expression);
-
+            
             var builder = new CommandBuilder(new NpgsqlCommand());
-
+            
             result.Apply(builder);
             
             builder.ToString().ShouldContain("d.mt_deleted and d.mt_deleted_at >");
+            builder.ToString().ShouldNotContain("?");
         }
 
         [Fact]
